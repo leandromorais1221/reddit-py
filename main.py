3c98de52e223771ffe902a4298174c2f28ee0ff4
Leandro Morais <leandromorais1221@gmail.com>
2026-04-02 10:50:55 -0300

    chore: atualizar estrutura de rotas e templates e adicionar bcrypt as dependencias

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-from routes import *
+from routes.feed_routes import *
+from routes.auth_routes import *
 
 if __name__ == "__main__":
     app.run()
